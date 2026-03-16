04a1849785eac05abb80ae57aa0207e1ea29caf0
NaelNunes <nathanaelnunesteodoro@gmail.com>
2026-03-15 22:16:09 -0300

    refactor: code cleanup and method structuring

--- app/src/main/java/com/example/appaula02classesdebuglogcat/MainActivity.java
+++ app/src/main/java/com/example/appaula02classesdebuglogcat/MainActivity.java
@@ -23,11 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtPessoa;
     private TextView txtNome;
     private TextView txtIdade;
-
     Pessoa pessoa;
     int contador;
-
-    int contadorDeCliques = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,52 +37,58 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        btnFabricarPessoa = findViewById(R.id.btnFabricarPessoa);
-        btnSair = findViewById(R.id.btnSair);
-        btnCriarPessoa = findViewById(R.id.btnCriarPessoa);
-
-        txtPessoa = findViewById(R.id.txtPessoa);
-        txtNome = findViewById(R.id.txtNome);
-        txtIdade = findViewById(R.id.txtIdade);
-
+        iniciarComponentesDeLayout();
 
         btnFabricarPessoa.setOnClickListener(v -> {
-            Toast.makeText(this, "Botão Fabricar Pessoa Clicado", Toast.LENGTH_LONG).show();
-
-            int idade = getIdade();
-            pessoa = new Pessoa("Marco", idade);
-            txtPessoa.setText(pessoa.toString());
+            metodoFabricarUmaPessoa();
         });
 
         btnSair.setOnClickListener(v ->
         {
-            contador++;
-            Log.d("eventos", "Botão Sair Clicado "+contador);
-            finish();
+            sairDoAplicativo();
         });
 
         btnCriarPessoa.setOnClickListener(v -> {
-            pessoa = new Pessoa();
-            pessoa.setNome("Marco");
-            pessoa.setIdade(getIdade());
-
-            txtPessoa.setText(pessoa.toString());
-            txtNome.setText(pessoa.getNome());
-            txtIdade.setText(String.valueOf(pessoa.getIdade()));
-
-            Toast.makeText(this, "Botão Criar Pessoa Clicado", Toast.LENGTH_LONG).show();
-
-            contador++;
-            Log.d("eventos", "Botão Criar Pessoa Clicado "+contador);
-            Log.i("eventos", "Nova pessoa criada: "+pessoa.toString());
-            Log.w("eventos", "Nome da pessoa: "+pessoa.getNome());
-            Log.v("eventos", "Idade da pessoa: "+pessoa.getIdade());
-
+            metodoParaCriarPessoa();s
         });
-
-
     }
 
+    private void metodoParaCriarPessoa() {
+        pessoa = new Pessoa();
+        pessoa.setNome("Marco");
+        pessoa.setIdade(getIdade());
+
+        txtPessoa.setText(pessoa.toString());
+        txtNome.setText(pessoa.getNome());
+        txtIdade.setText(String.valueOf(pessoa.getIdade()));
+
+        Toast.makeText(this, "Botão Criar Pessoa Clicado", Toast.LENGTH_LONG).show();
+
+        contador++;
+        Log.d("eventos", "Botão Criar Pessoa Clicado "+contador);
+        Log.i("eventos", "Nova pessoa criada: "+pessoa.toString());
+        Log.w("eventos", "Nome da pessoa: "+pessoa.getNome());
+        Log.v("eventos", "Idade da pessoa: "+pessoa.getIdade());
+    }
+    private void metodoFabricarUmaPessoa() {
+        Toast.makeText(this, "Botão Fabricar Pessoa Clicado", Toast.LENGTH_LONG).show();
+        int idade = getIdade();
+        pessoa = new Pessoa("Marco", idade);
+        txtPessoa.setText(pessoa.toString());
+    }
+    private void iniciarComponentesDeLayout() {
+        btnFabricarPessoa = findViewById(R.id.btnFabricarPessoa);
+        btnSair = findViewById(R.id.btnSair);
+        btnCriarPessoa = findViewById(R.id.btnCriarPessoa);
+        txtPessoa = findViewById(R.id.txtPessoa);
+        txtNome = findViewById(R.id.txtNome);
+        txtIdade = findViewById(R.id.txtIdade);
+    }
+    private void sairDoAplicativo() {
+        contador++;
+        Log.d("eventos", "Botão Sair Clicado "+contador);
+        finish();
+    }
     private static int getIdade() {
         Random idadeAleatoria = new Random();
         int min = 18;
